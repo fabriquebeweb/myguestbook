a28d04c76285c3c81c1620d8efc4d7b422881d2f
Alexandre Labsi <alex.lbs2001@gmail.com>
2021-07-27 12:32:42 +0000

    mayenne tien

---
+++ myguestbook.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * @package MyGuestBook
+ * @version 1
+ */
+/*
+Plugin Name: MyGuestBook
+Plugin URI: https://wordpress.org/plugins/myguestbook/
+Description: Rajoute un section livre d'or qui permet au client de laisser un commentaire et une note, attribué au restaurant
+Author: Yohan Beneito, Sidney Carlos, Raphaël Cima, Valentin Creuillenet, Alexandre Labsi, Jonathan Littardi, Maïalen Watrigant
+Version: 1
+Author URI: ∞∞∞∞∞∞∞∞∞∞∞∞∞
+*/
+
+// //link le style.css au plugin
+// function register_admin_style() {
+//     wp_register_style( 'admin_style', plugins_url('/assets/style.css', __FILE__), false, '1.0.0', 'all' );
+//     wp_enqueue_style( 'admin_style' );
+// }
+// add_action( 'admin_init', 'register_admin_style' );
+include_once('src/form.php');
+
+// Récupérer le nom de la table
+function getTableName() {
+    global $wpdb;
+
+    return $wpdb->prefix . 'myguestbook';
+}
+
+// Créer la table en BDD
+function db_create() {
+    global $wpdb;
+
+    $table_name = getTableName();
+    $charset_collate = $wpdb->get_charset_collate();
+
+    $sql = "CREATE TABLE $table_name (
+    id int NOT NULL AUTO_INCREMENT,
+    message text NOT NULL,
+    name tinytext,
+    time datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    PRIMARY KEY  (id)
+    ) $charset_collate;";
+
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    dbDelta( $sql );
+}
+
+// Appels lors de l'activation du plugin
+register_activation_hook( __FILE__, 'db_create' );
+
+// Crée et ajoute le menu à l'interface admin
+add_action('admin_menu', function() {
+    add_menu_page(
+        'MyGuestBook Settings',
+        'MyGuestBook',
+        'manage_options',
+        'myguestbook',
+        '',
+        'dashicons-awards',
+        null
+    );
+});
+
+
+
+
+
